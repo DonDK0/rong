da696524ca5869b1cfb8e5bd89b845c2a8bbb67e
DonDK0 <don.angsupapong@gmail.com>
2024-07-21 08:31:38 +0700

    add reset button

--- rongrean/app.py
+++ rongrean/app.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc,callback,Input, Output,dependencies
+from dash import Dash, html, dash_table, dcc,callback,Input, ctx, Output,dependencies
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -15,21 +15,27 @@ app.layout = html.Div(children=[
     html.H1(children='Student Distribution by Province'),
     dcc.Input(id='province-input', type='text', placeholder='Enter province name'),
     html.Button(id='submit-button', n_clicks=0, children='Search'),
-    dcc.Graph(id='province-graph')
+    html.Button(id='reset-button', n_clicks=0, children='Reset'),
+    dcc.Graph(id='province-graph',)
 ])
 
 # Define the callback to update the graph
 @app.callback(
     Output('province-graph', 'figure'),
-    [Input('submit-button', 'n_clicks')],
+    [Input('submit-button', 'n_clicks'),Input('reset-button', 'n_clicks')],
     [dependencies.State('province-input', 'value')]
 )
-def update_graph(n_clicks, province):
-    filtered_df = df[df['schools_province'] == province] if province else df
+def update_graph(n_clicks, resetbtn, province):
+    if "reset-button"  == ctx.triggered_id:
+        filtered_df = df
+    else:
+        filtered_df = df[df['schools_province'] == province] if province else df
     fig = px.bar(filtered_df, x='schools_province', y=['totalmale', 'totalfemale', 'totalstd'],
                  labels={'value': 'Number of Students', 'variable': 'Gender'},
                  barmode='group')
     return fig
+
+
 
 # Run the app
 if __name__ == '__main__':
